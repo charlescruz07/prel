8dcfc00bd28419702a238ae7c3b5b4c865a67bbd
Charles <Charles>
2017-07-19 00:03:12 +0800

    first

---
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -0,0 +1,59 @@
+package tech.hyperdev.scorekeeper.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import tech.hyperdev.scorekeeper.R;
+import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
+
+public class MainActivity extends AppCompatActivity{
+
+    int theme;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        theme = R.style.DAY_MODE;
+        replaceFragments(theme);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.whiteTheme:
+                theme = R.style.DAY_MODE;
+                replaceFragments(theme);
+                break;
+            case R.id.darkTheme:
+                theme = R.style.NIGHT_MODE;
+                replaceFragments(theme);
+                break;
+        }
+        return true;
+    }
+
+    public void replaceFragments(int theme){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.containerOne, new ScoreFragment().newInstance("Team 1",theme))
+                .commit();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.containerTwo, new ScoreFragment().newInstance("Team 2",theme))
+                .commit();
+    }
+}
---
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -0,0 +1,85 @@
+package tech.hyperdev.scorekeeper.fragments;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import tech.hyperdev.scorekeeper.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ScoreFragment extends Fragment{
+
+    private String teamName;
+    private TextView tvTeamName, tvScore;
+    private ImageButton btnPlus,btnMinus;
+    private int theme;
+
+
+    public ScoreFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        this.theme = getArguments().getInt("theme");
+        final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), theme);
+        LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
+        // Inflate the layout for this fragment
+        View rootView = localInflater.inflate(R.layout.fragment_score,container,false);
+        this.teamName = getArguments().getString("team");
+
+
+        tvTeamName = (TextView) rootView.findViewById(R.id.tvTeamName);
+        btnPlus = (ImageButton) rootView.findViewById(R.id.btnPlus);
+        btnMinus = (ImageButton) rootView.findViewById(R.id.btnMinus);
+        tvScore = (TextView) rootView.findViewById(R.id.tvScore);
+
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int score = Integer.parseInt(tvScore.getText().toString());
+                int finalScore = score + 1;
+                tvScore.setText(String.valueOf(finalScore));
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int score = Integer.parseInt(tvScore.getText().toString());
+                int finalScore = score - 1;
+                if(finalScore < 0){
+                    tvScore.setText(String.valueOf(0));
+                    finalScore = Integer.parseInt(tvScore.getText().toString());
+                }
+                tvScore.setText(String.valueOf(finalScore));
+            }
+        });
+
+        tvTeamName.setText(teamName);
+        return rootView;
+    }
+
+    public static ScoreFragment newInstance(String teamName, int theme) {
+
+        Bundle args = new Bundle();
+        args.putString("team", teamName);
+        args.putInt("theme",theme);
+        ScoreFragment fragment = new ScoreFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
